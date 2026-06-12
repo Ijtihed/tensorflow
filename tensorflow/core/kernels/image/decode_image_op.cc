11e55c830fd471956ce3d68916bae2b459613a56
Ricardo Torres <torresguzman@google.com>
2026-06-12 10:55:31 -0700

    Fix a potential memory corruption issue in WebP animation decoding.
    PiperOrigin-RevId: 931231419

--- tensorflow/core/kernels/image/decode_image_op.cc
+++ tensorflow/core/kernels/image/decode_image_op.cc
@@ -16,6 +16,7 @@ limitations under the License.
 // See docs in ../ops/image_ops.cc
 
 #include <cmath>
+#include <csetjmp>
 #include <cstdint>
 #include <cstdlib>
 #include <functional>
@@ -817,7 +818,7 @@ class DecodeImageV2Op : public OpKernel {
     const bool use_threads = (width * height > 1024 * 1024);
     uint8_t* buffer = webp::DecodeWebPAnimation(
         input,
-        [&](int num_frames, int width, int height, int channls) -> uint8_t* {
+        [&](int num_frames, int width, int height, int channels) -> uint8_t* {
           // If expand_animations is false, we want {height, width, channels}
           // otherwise, we want {num_frames, height, width, channels} even if
           // it's a single frame.
@@ -878,9 +879,8 @@ class DecodeImageV2Op : public OpKernel {
   }
 
  private:
-  void DecodeBMP(const uint8_t* input, const int row_size,
-                 uint8_t* const output, const int width, const int height,
-                 const int output_channels, const int input_channels,
+  void DecodeBMP(const uint8_t* input, int row_size, uint8_t* output, int width,
+                 int height, int output_channels, int input_channels,
                  bool top_down);
 
   int channels_ = 0;
@@ -901,10 +901,10 @@ REGISTER_KERNEL_BUILDER(Name("DecodeBmp").Device(DEVICE_CPU), DecodeImageV2Op);
 REGISTER_KERNEL_BUILDER(Name("DecodeWebP").Device(DEVICE_CPU), DecodeImageV2Op);
 REGISTER_KERNEL_BUILDER(Name("DecodeJxl").Device(DEVICE_CPU), DecodeImageV2Op);
 
-void DecodeImageV2Op::DecodeBMP(const uint8_t* input, const int row_size,
-                                uint8_t* const output, const int width,
-                                const int height, const int output_channels,
-                                const int input_channels, bool top_down) {
+void DecodeImageV2Op::DecodeBMP(const uint8_t* input, int row_size,
+                                uint8_t* output, int width, int height,
+                                int output_channels, int input_channels,
+                                bool top_down) {
   for (int i = 0; i < height; i++) {
     int src_pos;
     int dst_pos;
